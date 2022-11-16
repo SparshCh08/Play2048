5e734e7f3994be05b0ac3442c55d974e7bca2b5a
SparshCh08 <52466156+SparshCh08@users.noreply.github.com>
2022-11-17 02:34:38 +0530

    first commit

---
+++ src/components/cell.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../src/main.scss';
+import '../../src/styles.scss';
+
+const Cell = () => {
+    return (
+        <div className='cell'></div>
+    )
+}
+
+export default Cell
